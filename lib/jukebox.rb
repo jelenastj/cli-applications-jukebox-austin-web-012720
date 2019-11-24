06b8c86a057737d93f646a5e4c386c9daa54eb04
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-24 23:19:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,9 +11,7 @@ songs = [
 ]
 
 def help 
-  puts "I accept the following commands:"
-  
-  puts "- help : displays this help message"
+  puts "I accept the following commands:/n - help : displays this help message/n
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
